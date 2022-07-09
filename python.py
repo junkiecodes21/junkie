3a3bb2ea85d7fb39b6b8c1f8c30189ba687b0607
vineeth <vineethpalleti@outlook.com>
2022-07-09 22:25:56 +0100

    new python file added

---
+++ python.py
@@ -0,0 +1,33 @@
+# print ("this is vineeth")
+
+a=100
+
+
+# if a%2 == 0:
+#     print("its an even number")
+# else:
+#     print(" its an odd number")
+
+
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+
+    if n%2==0:
+        if n>=2 and n<=5:
+            print("Not Weird")
+        if n>=6 and n<=20:
+            print("Weird")
+        if n>20:
+            print("Not Weird")
+    else:
+        print("Weird")
