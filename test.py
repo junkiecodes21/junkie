09eb3eef4024d0b86985b116f83a5f2b10e4bf4a
vineeth <vineethpalleti@outlook.com>
2022-07-10 19:02:03 +0100

    changes made

--- test.py
+++ test.py
@@ -1,8 +1,21 @@
-a = 10
-user_name = ' vineeth'
+# # checking the even number 
 
-a=20
-b=30
-c=a+b
-print ("add" , c)
+# a=100
+
+# if a%2 == 0:
+#     print("this is even")
+# else: 
+#     print("this is odd")
+
+
+my_variable = input("What's your jersey number")
+
+print(type(my_variable))
+
+if  my_variable == "10":
+    print("Tendulkar")
+else: 
+    print("You're not tendulkar")
+
+
 
