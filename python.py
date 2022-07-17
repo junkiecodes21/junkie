f714b667efe471c4f97e6da8c29ebb5de5354e34
vineeth <vineethpalleti@outlook.com>
2022-07-17 08:32:50 +0100

    extended classs

--- python.py
+++ python.py
@@ -11,23 +11,35 @@ a=100
 
 #!/bin/python
 
-import math
-import os
-import random
-import re
-import sys
+# import math
+# import os
+# import random
+# import re
+# import sys
 
 
 
-if __name__ == '__main__':
-    n = int(raw_input().strip())
+# if __name__ == '__main__':
+#     n = int(raw_input().strip())
 
-    if n%2==0:
-        if n>=2 and n<=5:
-            print("Not Weird")
-        if n>=6 and n<=20:
-            print("Weird")
-        if n>20:
-            print("Not Weird")
-    else:
-        print("Weird")
+#     if n%2==0:
+#         if n>=2 and n<=5:
+#             print("Not Weird")
+#         if n>=6 and n<=20:
+#             print("Weird")
+#         if n>20:
+#             print("Not Weird")
+#     else:
+#         print("Weird")
+
+
+
+length = input("the length of the rectangle :")
+breadth = input("the breadth of the rectangle :")
+# area = input(int(length)*int(breadth))
+
+if length == breadth:
+    print("its a square")
+else:
+    print("its a rectangle")
+
